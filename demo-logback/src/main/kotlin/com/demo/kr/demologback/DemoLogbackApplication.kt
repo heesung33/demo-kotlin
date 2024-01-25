7cd6749e383ba704f5e0902762e8be41c8a01347
chs <chaeheesung33@gmail.com>
2024-01-25 15:13:44 +0900

    feat: logback 추가

--- demo-logback/src/main/kotlin/com/demo/kr/demologback/DemoLogbackApplication.kt
+++ demo-logback/src/main/kotlin/com/demo/kr/demologback/DemoLogbackApplication.kt
@@ -1,11 +1,13 @@
 package com.demo.kr.demologback
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class DemoLogbackApplication
-
+private val log = LoggerFactory.getLogger(DemoLogbackApplication::class.java)
 fun main(args: Array<String>) {
     runApplication<DemoLogbackApplication>(*args)
+    log.info("logbackTest: " + "success")
 }
