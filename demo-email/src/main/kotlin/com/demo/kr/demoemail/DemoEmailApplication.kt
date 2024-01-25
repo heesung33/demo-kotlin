5f9bdbd66042fccb70c5ad11884f2bbe0b404224
chs <chaeheesung33@gmail.com>
2024-01-25 14:56:52 +0900

    초기 파일들 stage로 commit & push

---
+++ demo-email/src/main/kotlin/com/demo/kr/demoemail/DemoEmailApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.kr.demoemail
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoEmailApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoEmailApplication>(*args)
+}
